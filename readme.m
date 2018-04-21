5ed6bd0979388f5958ab9c0bcd3d3d6637b4e012
Lê Viết Mẫn <manleviet@gmail.com>
2018-04-21 11:17:53 +0700

    Rename readme.txt to readme.m

---
+++ readme.m
@@ -0,0 +1,14 @@
+Dans ce rŽpertoire, il comprend des ŽlŽments suivants :
++ Code-source :
+	- VGI.zip : le logiciel VGI
+	- ConfDatabase.zip : pour crŽer le fichier de configuration de la base de donnŽes
+	- VGIinput.zip : pour crŽer le fichier de configuration vgi version 1.0
+	- VisualWordViewer.zip : un logiciel qui peut dessiner les mots visuels d'une image
+	- DrawVisualWord.zip : traverser sur un rŽpertoire des images pour dessiner les mots visuels
++ Database.zip :
+	- le rŽpertoire BW : il y a deux rŽpertoires Wang-MotVisuel (les images avec les mots visuels) et Wang-ppm (les images pas les mots visuels). Il y a encore un fichier db.conf.
+	- database01 : comprend deux rŽpertoires MS et SA. Ce sont les donnŽes du logiciel DOAN Tuan Anh
+	- cluster# : les donnŽes partitionnement du cluster #
++ Documents.zip : contenir les rŽfŽrences du TPE
++ Rapport.zip : les rapports et les slides d'exposŽ. Les fichiers .pages et .keynote sont les fichiers de logiciel iWork qui est un logiciel sur MAC OS X.
++ cluster.ods : une analyse de resultats de la partitionnement de DOAN Tuan Anh
