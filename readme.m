7a7eb5719c9f87ea4c60691510049218645f9331
Lê Viết Mẫn <manleviet@gmail.com>
2020-05-07 17:41:59 +0200

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,16 +1,24 @@
-# Graph Visualization for Interactive Image Retrieval
+Graph Visualization for Interactive Image Retrieval
+=================
 
 Dans ce repertoire, il comprend des elements suivants :
+
 + Code-source :
+
 	- VGI.zip : le logiciel VGI
 	- ConfDatabase.zip : pour creer le fichier de configuration de la base de donnees
 	- VGIinput.zip : pour creer le fichier de configuration vgi version 1.0
 	- VisualWordViewer.zip : un logiciel qui peut dessiner les mots visuels d'une image
 	- DrawVisualWord.zip : traverser sur un repertoire des images pour dessiner les mots visuels
+	
 + Database.zip :
+
 	- le repertoire BW : il y a deux repertoires Wang-MotVisuel (les images avec les mots visuels) et Wang-ppm (les images pas les mots visuels). Il y a encore un fichier db.conf.
 	- database01 : comprend deux repertoires MS et SA. Ce sont les donnees du logiciel DOAN Tuan Anh
 	- cluster# : les donnees partitionnement du cluster #
+	
 + Documents.zip : contenir les references du TPE
+
 + Rapport.zip : les rapports et les slides d'expose. Les fichiers .pages et .keynote sont les fichiers de logiciel iWork qui est un logiciel sur MAC OS X.
+
 + cluster.ods : une analyse de resultats de la partitionnement de DOAN Tuan Anh
